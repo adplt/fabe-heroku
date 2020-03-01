eb6542c101b1c0d327012b0b735208994a43411a
adplt <atriadplt@gmail.com>
2020-03-01 04:53:42 +0000

    parsing data to view

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('pages/index');
+  const drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 },
+  ];
+  const tagline = 'Any code of your own that you haven\'t looked at for six or more months might as well have been written by someone else.';
+
+  res.render('pages/index', {
+    drinks,
+    tagline,
+  });
 });
 
 // list page
